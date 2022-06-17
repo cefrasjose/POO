e014c1c02f09250c06b0d192d1bc53e785a3b143
Cefras José <cefras54@gmail.com>
2022-06-17 12:06:41 -0300

    Create Aula 02 - tempo main.cpp
    Main da classe Tempo

---
+++ Aula 02 - tempo main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tempo.h"
+
+using namespace std;
+
+int main(){
+  Tempo padrao1;
+  Tempo custom(11, 20, 36);
+
+
+  cout << "Hora padrao: ";
+  padrao1.imprime();
+
+  cout << "\nHora customizada: ";
+  custom.imprime();
+
+  padrao1.setTempo(11, 25, 00);
+  cout << "\nHora padrao atualizada: ";
+  padrao1.imprime();
+
+  return 0;
+}
