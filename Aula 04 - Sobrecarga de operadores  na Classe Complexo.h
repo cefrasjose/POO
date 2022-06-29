fa3650a213b2fd36568281902b5d002149116aee
Cefras José <cefras54@gmail.com>
2022-06-29 17:44:04 -0300

    Create Aula 04 - Sobrecarga de operadores  na Classe Complexo.h
    Aplicação inicial de sobrecarga de operadores na classe complexo.

---
+++ Aula 04 - Sobrecarga de operadores  na Classe Complexo.h
@@ -0,0 +1,22 @@
+#ifndef COMPLEXO_H
+#define COMPLEXO_H
+
+class Complexo 
+{
+private:
+  float re, im;
+public:
+  Complexo(float=0.0, float=0.0);
+  ~Complexo() { }
+
+  // interface
+  void print();
+  Complexo add(Complexo&);
+  Complexo sub(Complexo&);
+
+  // sobrecarga de operadores
+  Complexo operator+(Complexo&);
+  Complexo operator-(Complexo&);
+};
+
+#endif
