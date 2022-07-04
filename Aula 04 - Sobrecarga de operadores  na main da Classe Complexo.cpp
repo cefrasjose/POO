bd0a8bc74b94336fbc0b69e1cacb0897789fdbcf
Cefras José <cefras54@gmail.com>
2022-07-04 13:18:30 -0300

    Update Aula 04 - Sobrecarga de operadores  na main da Classe Complexo.cpp

--- Aula 04 - Sobrecarga de operadores  na main da Classe Complexo.cpp
+++ Aula 04 - Sobrecarga de operadores  na main da Classe Complexo.cpp
@@ -16,15 +16,29 @@ int main() {
   cout << endl;
 
   Complexo n3, n4;
+
+  if(!n3 && !n4)
+    cout << "\nn3 e n4 inicialmente iguais a 0.\n";
+  
   n3 = n1 + n2;
   n4 = n1 - n2;
 
-  cout << "n1 (add) = ";
+  cout << "n3 (add) = ";
   n3.print();
   cout << endl;
 
-  cout << "n2 (sub) = ";
+  cout << "n4 (sub) = ";
   n4.print();
+  cout << endl;
+
+  n1 = n2++;
+  cout << "Depois do incremento:\n";
+  cout << "n1 = ";
+  n1.print();
+  cout << endl;
+
+  cout << "n2 = ";
+  n2.print();
   cout << endl;
   
   return 0;
