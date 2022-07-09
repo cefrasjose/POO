5b7019ed40852461a5cb3abdfb22f76b1cd4d776
Cefras José <cefras54@gmail.com>
2022-07-09 12:32:47 -0300

    Create Aula 06 Herança classe professor.h
    Classe professor também herdando dados da classe pessoa.

---
+++ Aula 06 Herança classe professor.h
@@ -0,0 +1,19 @@
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+
+#include "pessoa.h"
+
+class Professor : public Pessoa {
+  private:
+    int siepe;
+    int categoria;
+    string instituto;
+  public:
+    Professor(string, string, int, int, string);
+    ~Professor() { }
+    int promover() { return ++categoria; }
+    void setInstituto(string i) { instituto = i; }
+    void imprime();
+};
+
+#endif
