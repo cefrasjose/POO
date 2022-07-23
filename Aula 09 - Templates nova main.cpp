ba170b87fe0d888f3668656b9727cfaee02cc2a3
Cefras José <cefras54@gmail.com>
2022-07-23 13:29:25 -0300

    Create Aula 09 - Templates nova main.cpp

---
+++ Aula 09 - Templates nova main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include "pilha.h"
+
+using namespace std;
+
+int main() {
+  Pilha<float> p{50};
+  float v = 0;
+
+  cout << "Inserindo itens na pilha: \n";
+  while(v != -1){
+    cin >> v;
+    p.push(v);
+  }
+
+  cout << "Removendo itens... \n";
+  while(!(p.is_empty())) {
+    p.pop(v);
+    cout << v << " ";
+  }
+
+  return 0;
+}
